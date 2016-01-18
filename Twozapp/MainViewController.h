a38fff3ff62cef346c2eee158391810cd19d8d13
PriyankaBalachandran <priyankabalachandran12@gmail.com>
2016-01-18 22:17:25 +0530

    Commit side swipe for like and dislike

--- Twozapp/MainViewController.h
+++ Twozapp/MainViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DraggableView.h"
 
-@interface MainViewController : UIViewController
+@interface MainViewController : UIViewController<DraggableViewDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imgViewProfilePic;
 @property (weak, nonatomic) IBOutlet UIView *viewName;
 @property (weak, nonatomic) IBOutlet UIView *viewBazi;
@@ -16,10 +17,18 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imgViewOnline;
 @property (weak, nonatomic) IBOutlet UIButton *btnNotIntrested;
 @property (weak, nonatomic) IBOutlet UIButton *btnIntrested;
+@property (weak, nonatomic) IBOutlet UIView *viewProfile;
 
 - (IBAction)actionSideMenu:(id)sender;
 - (IBAction)actionNotIntrested:(id)sender;
 - (IBAction)actionIntrested:(id)sender;
+
+
+-(void)cardSwipedLeft:(UIView *)card;
+-(void)cardSwipedRight:(UIView *)card;
+
+@property (retain,nonatomic)NSArray* exampleCardLabels; //%%% the labels the cards
+@property (retain,nonatomic)NSMutableArray* allCards;
 
 
 @end
--- Twozapp/MainViewController.m
+++ Twozapp/MainViewController.m
@@ -8,14 +8,24 @@
 
 #import "MainViewController.h"
 #import "RNFrostedSidebar.h"
+#import "DraggableView.h"
 
-@interface MainViewController ()<RNFrostedSidebarDelegate>
+static const int MAX_BUFFER_SIZE = 2; //%%% max number of cards loaded at any given time, must be greater than 1
+static const float CARD_HEIGHT = 386; //%%% height of the draggable card
+static const float CARD_WIDTH = 290;
+
+@interface MainViewController ()<RNFrostedSidebarDelegate>{
+    NSInteger cardsLoadedIndex; 
+    NSMutableArray *loadedCards;
+}
 @property (nonatomic, strong) NSMutableIndexSet *optionIndices;
 
 
 @end
 
 @implementation MainViewController
+@synthesize exampleCardLabels;
+@synthesize allCards;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -110,4 +120,103 @@
     }];
 
 }
+
+
+-(DraggableView *)createDraggableViewWithDataAtIndex:(NSInteger)index
+{
+    DraggableView *draggableView = [[DraggableView alloc]initWithFrame:CGRectMake((self.view.frame.size.width - _viewProfile.frame.size.width)/2, (self.view.frame.size.height - _viewProfile.frame.size.height)/2, _viewProfile.frame.size.width, _viewProfile.frame.size.height)];
+    draggableView.information.text = [exampleCardLabels objectAtIndex:index]; //%%% placeholder for card-specific information
+    draggableView.delegate = self;
+    return draggableView;
+}
+
+//%%% loads all the cards and puts the first x in the "loaded cards" array
+-(void)loadCards
+{
+    if([exampleCardLabels count] > 0) {
+        NSInteger numLoadedCardsCap =(([exampleCardLabels count] > MAX_BUFFER_SIZE)?MAX_BUFFER_SIZE:[exampleCardLabels count]);
+        //%%% if the buffer size is greater than the data size, there will be an array error, so this makes sure that doesn't happen
+        
+        //%%% loops through the exampleCardsLabels array to create a card for each label.  This should be customized by removing "exampleCardLabels" with your own array of data
+        for (int i = 0; i<[exampleCardLabels count]; i++) {
+            DraggableView* newCard = [self createDraggableViewWithDataAtIndex:i];
+            [allCards addObject:newCard];
+            
+            if (i<numLoadedCardsCap) {
+                //%%% adds a small number of cards to be loaded
+                [loadedCards addObject:newCard];
+            }
+        }
+        
+        //%%% displays the small number of loaded cards dictated by MAX_BUFFER_SIZE so that not all the cards
+        // are showing at once and clogging a ton of data
+        for (int i = 0; i<[loadedCards count]; i++) {
+            if (i>0) {
+                [_viewProfile insertSubview:[loadedCards objectAtIndex:i] belowSubview:[loadedCards objectAtIndex:i-1]];
+            } else {
+                [_viewProfile addSubview:[loadedCards objectAtIndex:i]];
+            }
+            cardsLoadedIndex++; //%%% we loaded a card into loaded cards, so we have to increment
+        }
+    }
+}
+
+#warning include own action here!
+//%%% action called when the card goes to the left.
+// This should be customized with your own action
+-(void)cardSwipedLeft:(UIView *)card;
+{
+    //do whatever you want with the card that was swiped
+    //    DraggableView *c = (DraggableView *)card;
+    
+    [loadedCards removeObjectAtIndex:0]; //%%% card was swiped, so it's no longer a "loaded card"
+    
+    if (cardsLoadedIndex < [allCards count]) { //%%% if we haven't reached the end of all cards, put another into the loaded cards
+        [loadedCards addObject:[allCards objectAtIndex:cardsLoadedIndex]];
+        cardsLoadedIndex++;//%%% loaded a card, so have to increment count
+        [_viewProfile insertSubview:[loadedCards objectAtIndex:(MAX_BUFFER_SIZE-1)] belowSubview:[loadedCards objectAtIndex:(MAX_BUFFER_SIZE-2)]];
+    }
+}
+
+#warning include own action here!
+//%%% action called when the card goes to the right.
+// This should be customized with your own action
+-(void)cardSwipedRight:(UIView *)card
+{
+    //do whatever you want with the card that was swiped
+    //    DraggableView *c = (DraggableView *)card;
+    
+    [loadedCards removeObjectAtIndex:0]; //%%% card was swiped, so it's no longer a "loaded card"
+    
+    if (cardsLoadedIndex < [allCards count]) { //%%% if we haven't reached the end of all cards, put another into the loaded cards
+        [loadedCards addObject:[allCards objectAtIndex:cardsLoadedIndex]];
+        cardsLoadedIndex++;//%%% loaded a card, so have to increment count
+        [_viewProfile insertSubview:[loadedCards objectAtIndex:(MAX_BUFFER_SIZE-1)] belowSubview:[loadedCards objectAtIndex:(MAX_BUFFER_SIZE-2)]];
+    }
+    
+}
+
+//%%% when you hit the right button, this is called and substitutes the swipe
+-(void)swipeRight
+{
+    DraggableView *dragView = [loadedCards firstObject];
+    dragView.overlayView.mode = GGOverlayViewModeRight;
+    [UIView animateWithDuration:0.2 animations:^{
+        dragView.overlayView.alpha = 1;
+    }];
+    [dragView rightClickAction];
+}
+
+//%%% when you hit the left button, this is called and substitutes the swipe
+-(void)swipeLeft
+{
+    DraggableView *dragView = [loadedCards firstObject];
+    dragView.overlayView.mode = GGOverlayViewModeLeft;
+    [UIView animateWithDuration:0.2 animations:^{
+        dragView.overlayView.alpha = 1;
+    }];
+    [dragView leftClickAction];
+}
+
+
 @end
