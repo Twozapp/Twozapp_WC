c88e8fa93ca75f519868e80c2ece49968ea5040d
PriyankaBalachandran <priyankabalachandran12@gmail.com>
2016-01-22 01:23:05 +0530

    Commit My Bazi View Controller

--- Twozapp/MyBaziViewController.h
+++ Twozapp/MyBaziViewController.h
@@ -9,5 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface MyBaziViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblName;
+@property (weak, nonatomic) IBOutlet UILabel *lblGender;
+@property (weak, nonatomic) IBOutlet UILabel *lblBirthday;
+@property (weak, nonatomic) IBOutlet UIButton *btnHorse;
+@property (weak, nonatomic) IBOutlet UIButton *btnOx;
+@property (weak, nonatomic) IBOutlet UIButton *btnDragan;
+@property (weak, nonatomic) IBOutlet UIView *view4;
+@property (weak, nonatomic) IBOutlet UIButton *btnSnake;
+@property (weak, nonatomic) IBOutlet UIView *view3;
+@property (weak, nonatomic) IBOutlet UILabel *lblTextDescription;
+@property (weak, nonatomic) IBOutlet UIView *view2;
+@property (weak, nonatomic) IBOutlet UIView *view1;
+@property (weak, nonatomic) IBOutlet UIView *view5;
+@property (weak, nonatomic) IBOutlet UIView *view6;
+@property (weak, nonatomic) IBOutlet UIView *view7;
+@property (weak, nonatomic) IBOutlet UIView *view8;
 
 @end
